9dfcdc5f4f20e566f49f483901b4b083e350a556
Matthew McKeon <matthewmckeon@Matthews-MacBook-Pro.local>
2021-02-20 16:36:03 -0500

    build attempt

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ app.use(cors());
 // ROUTES
 app.use('/rounds', roundRouter);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // START APP
 const port = process.env.port || 5000;
 app.listen(port, () => {
