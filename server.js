7485903f28551c9bf25e611f017fc5ba93e74858
Matthew McKeon <matthewmckeon@matthews-mbp.myfiosgateway.com>
2021-02-20 19:00:59 -0500

    hopefully this works

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // START APP
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
